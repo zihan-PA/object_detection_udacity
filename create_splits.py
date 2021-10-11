09c86e4223c50cf164477166fd7d6120bafd8899
Zihan Zhang <zihan@perceptiveautomata.com>
2021-10-11 16:26:40 -0400

    update the split dataset code

--- create_splits.py
+++ create_splits.py
@@ -16,8 +16,29 @@ def split(data_dir):
     args:
         - data_dir [str]: data directory, /mnt/data
     """
-    # TODO: Implement function
-    
+    dest_ratios = [("train", 0.7), ("validation", 0.2), ("test", 0.1)]
+
+    tfRecordFiles = []
+    for tfRecordFile in glob.glob(os.path.join(data_dir,'*.tfrecord')):
+        tfRecordFiles.append(tfRecordFile)
+
+    random.shuffle(tfRecordFiles)
+
+    total_count = len(tfRecordFiles)
+    cur_ratio = 0.0
+    for dest_dir_name, ratio in dest_ratios:
+        start_index = int(round(cur_ratio * total_count))
+        cur_ratio += ratio
+        end_index = int(round(cur_ratio * total_count))
+        if end_index == total_count:
+            end_index = None
+        dest_dir = os.path.join(data_dir, dest_dir_name)
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+
+        for tfRecordFile in tfRecordFiles[start_index : end_index]:
+            os.rename(tfRecordFile, os.path.join(dest_dir, os.path.basename(tfRecordFile)))
+
 
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
